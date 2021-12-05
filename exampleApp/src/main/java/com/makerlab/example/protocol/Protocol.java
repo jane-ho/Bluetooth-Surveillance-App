956079cdc6b3a2387287768de08786e1cd82e391
jane-hhc <u3557044@connect.hku.hk>
2021-12-05 22:33:21 +0800

    Removed spinner for protocols

--- exampleApp/src/main/java/com/makerlab/example/protocol/Protocol.java
+++
@@ -1,9 +0,0 @@
-package com.makerlab.example.protocol;
-
-public class Protocol {
-    static public final int PLAIN_TEXT = 0;
-    static public final int GOBLE = 1;
-    static public final int VORPAL = 2;
-    static public final int RobotArmGcode = 3;
-    static public final int DrawBotScaraGcode = 4;
-}
--- exampleApp/src/main/java/com/makerlab/example/ui/MainFragmentControl.java
+++ exampleApp/src/main/java/com/makerlab/example/ui/MainFragmentControl.java
@@ -11,30 +11,20 @@ import android.widget.Button;
 import androidx.fragment.app.Fragment;
 
 import com.makerlab.bt.BluetoothConnect;
-import com.makerlab.example.protocol.DrawBotScaraGcode;
-import com.makerlab.example.protocol.GoBLE;
 import com.makerlab.example.protocol.PlainTextProtocol;
-import com.makerlab.example.protocol.Protocol;
-import com.makerlab.example.protocol.Vorpal;
-import com.makerlab.example.widgets.ProtocolSelectSpinner;
 
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainFragmentControl extends Fragment implements
-        View.OnClickListener, AdapterView.OnItemSelectedListener {
+public class MainFragmentControl extends Fragment implements View.OnClickListener {
     static private String LOG_TAG = MainFragmentControl.class.getSimpleName();
     static public final boolean D = BuildConfig.DEBUG;
 
     private BluetoothConnect mBluetoothConnect;
     private Timer mDataSendTimer = null;
-    private int mProtocolId = -1;
-    private GoBLE mGoBLE;
     private PlainTextProtocol mPlainTextProtocol;
-    private Vorpal mVorpal;
-    private DrawBotScaraGcode mDrawBot;
     private Queue<byte[]> mQueue = new LinkedList<>();
     private final int buttionID[] = {
             0, // dummy value
@@ -55,10 +45,7 @@ public class MainFragmentControl extends Fragment implements
     public void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         Log.e(LOG_TAG, "onCreate()");
-        mGoBLE = new GoBLE();
-        mVorpal = new Vorpal();
         mPlainTextProtocol = new PlainTextProtocol();
-        mDrawBot = new DrawBotScaraGcode();
 
     }
 
@@ -72,8 +59,6 @@ public class MainFragmentControl extends Fragment implements
             }
             Log.e(LOG_TAG, "onCreateView()");
         }
-        ProtocolSelectSpinner protocolSpinner = rootView.findViewById(R.id.protocolSpinner);
-        protocolSpinner.setOnItemSelectedListener(this);
         return rootView;
     }
 
@@ -111,105 +96,9 @@ public class MainFragmentControl extends Fragment implements
         }
         //view.setEnabled(true);
         synchronized (mQueue) {
-            switch (mProtocolId) {
-                case Protocol.PLAIN_TEXT:
-                    mQueue.add(mPlainTextProtocol.getPayload(buttonClicked));
-                    mQueue.add(mPlainTextProtocol.getPayload(0));
-                    if (D)
-                        Log.e(LOG_TAG, "onClick() : plaintext : button clicked " + buttonClicked);
-                    break;
-                case Protocol.GOBLE:
-                    final byte[] buttonMap = {0, 1, 2, 3, 4, 7, 8, 11, 12}; // remap the buttonID
-                    byte[] buttonPressed = {buttonMap[buttonClicked]};
-                    // move at one interval for  button pressed
-                    mQueue.add(mGoBLE.getPayload(127, 127, buttonPressed));
-                    // stop at next interval for button released
-                    mQueue.add(mGoBLE.getPayload(127, 127, null));
-                    if (D)
-                        Log.e(LOG_TAG, "onClick() : goble : button clicked " + buttonMap[buttonClicked]);
-                    break;
-                case Protocol.VORPAL:
-                    final byte[][] locomotions = {null, mVorpal.goForward(), mVorpal.goRight(),
-                            mVorpal.goBackward(), mVorpal.goLeft(),
-                            mVorpal.stomp()};
-                    if (buttonClicked >=0 && buttonClicked<locomotions.length && locomotions[buttonClicked] != null) {
-                        mQueue.add(locomotions[buttonClicked]);
-                    }
-                    if (D)
-                        Log.e(LOG_TAG, "onClick() : vorpal : button clicked " + buttonClicked);
-                    break;
-                case Protocol.DrawBotScaraGcode:
-                    int feedrate = 800;
-                    int interval = 10;
-                    if (buttonClicked < 5) {
-                        mQueue.add(mDrawBot.setRelativePositioning());
-                    }
-                    switch (buttonClicked) {
-                        case 1:
-                            mQueue.add(mDrawBot.moveY(interval, feedrate));
-                            break;
-                        case 2:
-                            mQueue.add(mDrawBot.moveX(interval, feedrate));
-                            break;
-                        case 3:
-                            mQueue.add(mDrawBot.moveY(-interval, feedrate));
-                            break;
-                        case 4:
-                            mQueue.add(mDrawBot.moveX(-interval, feedrate));
-                            break;
-                        case 5:
-                            byte[][] gcodes = mDrawBot.goHomePosition();
-                            for (int i = 0; i < gcodes.length; i++) {
-                                mQueue.add(gcodes[i]);
-                            }
-                            break;
-                    }
-                    if (buttonClicked < 5) {
-                        mQueue.add(mDrawBot.setAbsolutePositioning());
-                    }
-                    if (D)
-                        Log.e(LOG_TAG, "onClick() : drawbot : button clicked " + buttonClicked);
-                    break;
-                default:
-                    if (D)
-                        Log.e(LOG_TAG, "onClick() : protocol not implemented!");
-            }
+            mQueue.add(mPlainTextProtocol.getPayload(buttonClicked));
+            mQueue.add(mPlainTextProtocol.getPayload(0));
         }
-
-    }
-
-    //AdapterView.OnItemSelectedListener of spinner view
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        synchronized (mQueue) {
-            switch (position) {
-                case Protocol.PLAIN_TEXT:
-                    mQueue.clear();
-                    mProtocolId = Protocol.PLAIN_TEXT;
-                    break;
-                case Protocol.GOBLE:
-                    mQueue.clear();
-                    mProtocolId = Protocol.GOBLE;
-                    break;
-                case Protocol.VORPAL:
-                    mQueue.clear();
-                    mProtocolId = Protocol.VORPAL;
-                    break;
-                case Protocol.DrawBotScaraGcode:
-                    mQueue.clear();
-                    mProtocolId = Protocol.DrawBotScaraGcode;
-                    break;
-                default:
-                    mProtocolId = -1;
-            }
-        }
-        if (D)
-            Log.e(LOG_TAG, "onItemSelected()");
-    }
-
-    //AdapterView.OnItemSelectedListener of spinner view
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 
